180a0f98d9a75b3a11dfe5c5d8e9e0b135d03bc0
jetblacksavior <coolanimeguy666@gmail.com>
2023-01-03 15:26:14 -0700

    bruh

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int stoi(int len, char *str){
+    //iterate through str
+    int ret = 0; 
+    for(int x = 0 ; x != len; x++){
+        int temp =0;
+        temp = str[x] -'0';
+        for(  int f = (len-1)-x; f !=0; f--){
+            temp *=10;
+            //printf("%c\n", )
+        }
+        printf("%i\n", temp);
+        ret += temp;
+    }
+
+    return 0 ;
+}
+
+int main(void){
+    int val = stoi(3, "123");
+}
