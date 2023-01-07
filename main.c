a7f5f9f8e29503def5d7111f1eb0b1c6c8120b5e
jetblacksavior <coolanimeguy666@gmail.com>
2023-01-06 17:04:59 -0700

    kljkl;

--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
-int stoi(int len, char *str){
-    //iterate through str
-    int ret = 0; 
-    for(int x = 0 ; x != len; x++){
-        int temp =0;
-        temp = str[x] -'0';
-        for(  int f = (len-1)-x; f !=0; f--){
-            temp *=10;
-            //printf("%c\n", )
+int myAtoi(char * s){
+    char* ptr = s;
+    int len= 0;
+    int ret= 0;
+    int sign = 0;
+    while(ptr[len]!=0){
+        if(ptr[len]=='-'){
+            sign =1;
         }
-        printf("%i\n", temp);
-        ret += temp;
+        len++;
     }
 
-    return 0 ;
-}
 
+    
+    
+    for(int  x =0; x != len; x++){
+        int temp =0;
+        temp = (s[x]-'0' >= 0 && s[x]-'0'<=9)?  s[x]-'0': -1;
+        printf("%i is temp\n", temp);
+        //temp = s[x] -'0';
+        if(temp ==-1||!temp){
+
+        }
+        else{
+        
+            for(  int f = (len-2)-x; f !=0; f--){
+                temp *=10;
+            }
+            ret += temp;
+            printf("%i is return\n", ret);
+        }
+        
+        
+    }
+    if(sign ==1){
+        ret*=-1;
+    }
+    return ret;
+}
 int main(void){
-    int val = stoi(3, "123");
+    int val = myAtoi("-42 ");
+    printf("return is = %i", val);
 }
